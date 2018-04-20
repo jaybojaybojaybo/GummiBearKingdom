497e602fe9165d25f9184496c7f2088cd51df247
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-20 09:02:30 -0700

    add Product and Category classes

---
+++ GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
