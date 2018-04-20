ee7321b9ab7551b00b9c5b5d0823f3396819f156
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-20 09:32:29 -0700

    format Products view

--- GummiBearKingdom/Models/Product.cs
+++ GummiBearKingdom/Models/Product.cs
@@ -14,6 +14,8 @@ namespace GummiBearKingdom.Models
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
     }
