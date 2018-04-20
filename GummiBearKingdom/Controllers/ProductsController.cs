e055c5e66fc845a3b991aeb3edc0c8d93b801c59
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-20 10:37:14 -0700

    add Create Method to controller and Create View  to  views for product

--- GummiBearKingdom/Controllers/ProductsController.cs
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummiBearKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,41 @@ namespace GummiBearKingdom.Controllers
 {
     public class ProductsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly GummiDbContext _context;
+
+        public ProductsController(GummiDbContext context)
         {
+            _context = context;
+        }
+
+
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            var gummiDbContext = _context.Products.Include(p => p.Category);
+            return View(await gummiDbContext.ToListAsync());
+        }
+
+        //GET: Product/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryName"] = new SelectList(_context.Categories, "CategoryId", "Name");
             return View();
+        }
+
+        //POST: Product/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProductId, Name, Description, Price, CatagoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            return View(product);
         }
     }
 }
