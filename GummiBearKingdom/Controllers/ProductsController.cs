b56cb379fe1be0108b4ca188d96462da9f2f2b87
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-27 22:08:55 -0700

    add Review Controller and Views - still debugging

--- GummiBearKingdom/Controllers/ProductsController.cs
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -14,70 +14,70 @@ namespace GummiBearKingdom.Controllers
     public class ProductsController : Controller
     {
         //Beginning of MockDatabase setup
-        public IGummiRepository productRepo;
+        //    public IGummiRepository productRepo;
 
-        public ProductsController(IGummiRepository repo = null)
-        {
-            if (repo == null)
-            {
-                this.productRepo = new EFProductRepository();
-            }
-            else
-            {
-                this.productRepo = repo;
-            }
-        }
+        //    public ProductsController(IGummiRepository repo = null)
+        //    {
+        //        if (repo == null)
+        //        {
+        //            this.productRepo = new EFProductRepository();
+        //        }
+        //        else
+        //        {
+        //            this.productRepo = repo;
+        //        }
+        //    }
 
-        public ViewResult Index()
-        {
-            return View(productRepo.Products.ToList());
-        }
+        //    public ViewResult Index()
+        //    {
+        //        return View(productRepo.Products.ToList());
+        //    }
 
-        public IActionResult Details(int id)
-        {
-            Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
-            return View(thisProduct);
-        }
+        //    public IActionResult Details(int id)
+        //    {
+        //        Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
+        //        return View(thisProduct);
+        //    }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //    public IActionResult Create()
+        //    {
+        //        return View();
+        //    }
 
-        [HttpPost]
-        public IActionResult Create(Product product)
-        {
-            productRepo.Save(product);
-            return RedirectToAction("Index");
-        }
+        //    [HttpPost]
+        //    public IActionResult Create(Product product)
+        //    {
+        //        productRepo.Save(product);
+        //        return RedirectToAction("Index");
+        //    }
 
-        public IActionResult Edit(int id)
-        {
-            Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
-            return View(thisProduct);
-        }
+        //    public IActionResult Edit(int id)
+        //    {
+        //        Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
+        //        return View(thisProduct);
+        //    }
 
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            productRepo.Edit(product);
-            return RedirectToAction("Index");
-        }
+        //    [HttpPost]
+        //    public IActionResult Edit(Product product)
+        //    {
+        //        productRepo.Edit(product);
+        //        return RedirectToAction("Index");
+        //    }
 
-        public IActionResult Delete(int id)
-        {
-            Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
-            return View(thisProduct);
-        }
+        //    public IActionResult Delete(int id)
+        //    {
+        //        Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
+        //        return View(thisProduct);
+        //    }
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
-            productRepo.Remove(thisProduct);
-            return RedirectToAction("Index");
-        }
-    }
+        //    [HttpPost, ActionName("Delete")]
+        //    public IActionResult DeleteConfirmed(int id)
+        //    {
+        //        Product thisProduct = productRepo.Products.FirstOrDefault(x => x.ProductId == id);
+        //        productRepo.Remove(thisProduct);
+        //        return RedirectToAction("Index");
+        //    }
+        //}
 
         //End of MockDatabase setup
 
@@ -89,101 +89,101 @@ namespace GummiBearKingdom.Controllers
 
 
 
-        //private readonly GummiDbContext _context;
+        private readonly GummiDbContext _context;
 
-        //public ProductsController(GummiDbContext context)
-        //{
-        //    _context = context;
-        //}
+        public ProductsController(GummiDbContext context)
+        {
+            _context = context;
+        }
 
 
-        //// GET: Product
-        //public async Task<IActionResult> Index()
-        //{
-        //    var gummiDbContext = _context.Products.Include(p => p.Category);
-        //    return View(await gummiDbContext.ToListAsync());
-        //}
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            var gummiDbContext = _context.Products.Include(p => p.Category);
+            return View(await gummiDbContext.ToListAsync());
+        }
 
-        ////GET: Product/Create
-        //public IActionResult Create()
-        //{
-        //    ViewData["CategoryName"] = new SelectList(_context.Categories, "CategoryId", "Name");
-        //    return View();
-        //}
+        //GET: Product/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryName"] = new SelectList(_context.Categories, "CategoryId", "Name");
+            return View();
+        }
 
-        ////POST: Product/Create
-        //[HttpPost]
-        //public async Task<IActionResult> Create([Bind("ProductId, Name, Description, Price, CategoryId")] Product product)
-        //{
-        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
-        //    _context.Add(product);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
+        //POST: Product/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProductId, Name, Description, Price, CategoryId")] Product product)
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
 
-        ////GET: Product/Details/id
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    var product = await _context.Products
-        //        .Include(p => p.Category)
-        //        .SingleOrDefaultAsync(m => m.ProductId == id);
-        //    return View(product);
-        //}
+        //GET: Product/Details/id
+        public async Task<IActionResult> Details(int? id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .SingleOrDefaultAsync(m => m.ProductId == id);
+            return View(product);
+        }
 
-        ////GET: Product/Delete/id
-        //public async Task<IActionResult> Delete(int id)
-        //{
-        //    var product = await _context.Products
-        //        .Include(p => p.Category)
-        //        .SingleOrDefaultAsync(m => m.ProductId == id);
-        //    return View(product);
-        //}
+        //GET: Product/Delete/id
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .SingleOrDefaultAsync(m => m.ProductId == id);
+            return View(product);
+        }
 
-        ////POST: Product/Delete/id
-        //[HttpPost, ActionName("Delete")]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var product = await _context.Products.SingleOrDefaultAsync(p => p.ProductId == id);
-        //    _context.Products.Remove(product);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
+        //POST: Product/Delete/id
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(p => p.ProductId == id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
 
-        ////GET: Product/Edit/id
-        //public async Task<IActionResult> Edit(int id)
-        //{
-        //    var product = await _context.Products
-        //        .Include(p => p.Category)
-        //        .SingleOrDefaultAsync(c => c.ProductId == id);
-        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
+        //GET: Product/Edit/id
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .SingleOrDefaultAsync(c => c.ProductId == id);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
 
-        //    return View(product);
-        //}
+            return View(product);
+        }
 
-        ////POST: Product/Edit/id
-        //[HttpPost]
-        //public async Task<IActionResult> Edit(int id, [Bind("ProductId, Name, Description, Price, CategoryId")] Product product)
-        //{
-        //    _context.Update(product);
-        //    await _context.SaveChangesAsync();
-        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
-        //    return RedirectToAction("Index");
-        //}
+        //POST: Product/Edit/id
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId, Name, Description, Price, CategoryId")] Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            return RedirectToAction("Index");
+        }
 
-        ////GET: Product/DeleteAll
-        //public async Task<IActionResult> DeleteAll()
-        //{
-        //    var gummiDbContext = _context.Products.Include(p => p.Category);
-        //    return View(await gummiDbContext.ToListAsync());
-        //}
+        //GET: Product/DeleteAll
+        public async Task<IActionResult> DeleteAll()
+        {
+            var gummiDbContext = _context.Products.Include(p => p.Category);
+            return View(await gummiDbContext.ToListAsync());
+        }
 
-        ////POST: Product/Delete/id
-        //[HttpPost, ActionName("DeleteAll")]
-        //public async Task<IActionResult> DeleteAllConfirmed()
-        //{
-        //    _context.Database.ExecuteSqlCommand("TRUNCATE TABLE products");
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("Index", "Home");
-        //}
-    //}
+        //POST: Product/Delete/id
+        [HttpPost, ActionName("DeleteAll")]
+        public async Task<IActionResult> DeleteAllConfirmed()
+        {
+            _context.Database.ExecuteSqlCommand("TRUNCATE TABLE products");
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
---
+++ GummiBearKingdom/Controllers/ReviewsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummiBearKingdom.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly GummiDbContext _context;
+
+        public ReviewsController(GummiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(int? productId)
+        {
+            var gummiDbContext = _context.Reviews.Include(r => r.Product);
+            return View(await gummiDbContext.ToListAsync());
+        }
+
+        //GET: Product/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductName"] = new SelectList(_context.Products, "ProductId", "Name");
+            return View();
+        }
+
+        //POST: Product/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ReviewId, Author, Content_Body, rating, ProductId")] Review review)
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", review.ProductId);
+            _context.Add(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
