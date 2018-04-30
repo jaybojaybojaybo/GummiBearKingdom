9388b722c4c41dde97fa4e66037220d671976fd7
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-29 19:27:43 -0700

    pass test to make sure Review Content_Body is under 255 characters

--- UnitTestProject1/GummiBearKingdomTests.cs
+++ UnitTestProject1/GummiBearKingdomTests.cs
@@ -142,5 +142,28 @@ namespace GummiBearKingdom.Tests
             Assert.AreEqual(reviewList.Count, 0);
 
         }
+
+        [TestMethod]
+        public void RatingValue_FiltersWrongContentBodyInput_Error()
+        {
+            //Arrange
+            GummiTestDbContext context = new GummiTestDbContext();
+            Review review1 = new Review();
+            review1.ReviewId = 1;
+            review1.Author = "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win - win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.User generated content in real - time will have multiple touchpoints for offshoring. Capitalize on low hanging fruit to identify a ballpark value added activity to beta test.Override the digital divide with additional clickthroughs from DevOps.Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.";
+            review1.Content_Body = "awesome";
+            review1.rating = 5;
+            review1.ProductId = 1;
+
+            //Act
+            context.SaveChanges();
+            List<Review> reviewList = context.Reviews.ToList();
+
+            //Assert
+            Assert.AreEqual(reviewList.Count, 0);
+
+        }
+
+
     }
 }
